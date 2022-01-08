15967e1789799aab6854722e7e2f595f78fb3655
Elijah Miernicki <miernickielijah@gmail.com>
2022-01-08 16:36:51 -0600

    personal background picture added, changed Nav bar

--- my-app/src/pages/home/index.js
+++ my-app/src/pages/home/index.js
@@ -1,31 +1,20 @@
-import Cube from './Cube.mp4'
-import Logo from './logo.png'
+import Eli from './Eli2.png'
 import './style.css'
 
-function Home () {
-    return(
+function Home() {
+    return (
         <main className="page">
-        <div className="overlay"></div>
-        <video id="vid" playsInline autoPlay muted loop>
-            <source src= {Cube} type="video/mp4"></source>
-        </video>
-
-        <header id="centerAll">
-            <section
-                className="container-fluid h-100 d-flex justify-content-center home animate__animated animate__fadeIn animate__slow">
-                <div className="d-flex h-100 text-center align-items-center main">
-                    <div className="w-100">
-                        <img id="logo" src={Logo} alt=""></img>
-                        <button onClick={clicker} type="button" className="btn" id="explore"><span id="animate"
-                                data-text="Explore...">Explore</span></button>
+            <img className="d-block mx-lg-auto img-fluid" src={Eli} alt='me'></img>
+            <header id="centerAll">
+                <section
+                    className="container-fluid h-100 d-flex justify-content-center home animate__animated animate__fadeIn animate__slow">
+                    <div className="d-flex h-100 text-center align-items-center main">
                     </div>
-                </div>
-            </section>
-        </header>
-    </main>
+                </section>
+            </header>
+        </main>
     )
 }
-
 
 //display NAV on click//
 function open() {
