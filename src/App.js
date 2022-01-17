b15d2849d0ba1dee01cd9533968363792f00dffe
Elijah Miernicki <miernickielijah@gmail.com>
2022-01-17 16:50:53 -0600

    added switch statement for app.js routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/home';
 import Nav from './components/nav';
 import Contact from "./components/contact/contact";
 import Projects from './pages/projects';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
@@ -14,20 +14,22 @@ function App() {
         <Nav />
       </div>
       <div className="App">
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/projects">
-          <Projects />
-        </Route>
-        <Route exact path="/services">
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/services">
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
