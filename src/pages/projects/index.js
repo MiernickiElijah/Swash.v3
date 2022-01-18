f6bb873d7009f06c92404cbca5f6b05f56f2a170
Elijah Miernicki <miernickielijah@gmail.com>
2022-01-18 11:27:32 -0600

    added BORN project, image, and re-ordered project list

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -5,6 +5,7 @@ import magic from './magic.jpg'
 import poggers from './poggers.jpg'
 import focus from './focus.jpg'
 import storm from './storm.jpg'
+import newdle from './newdle.PNG'
 import * as AireactIcons from "react-icons/ai";
 
 function Projects() {
@@ -14,7 +15,7 @@ function Projects() {
                 {/* <!--PROJECTS--> */}
                 <div className="page" data-page="3">
                     <section id="deck">
-                        {/* <!--P4--> */}
+                        {/* <!--p1--> */}
                         <article className="container col-xxl-8 px-4 py-5">
                             <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                                 <div className="col-lg-6">
@@ -33,7 +34,52 @@ function Projects() {
                             </div>
                         </article>
 
-                        {/* <!--P1--> */}
+
+                        {/* <!--p2--> */}
+                        <article className="container col-xxl-8 px-4 py-5">
+                            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                                <div className="col-10 col-sm-8 col-lg-6">
+                                    <a href="https://born001.herokuapp.com/" target="_blank" rel="noreferrer">
+                                        <img className="d-block mx-lg-auto img-fluid pics"
+                                            src={newdle} alt="newdle"></img>
+                                    </a>
+                                </div>
+                                <div className="col-lg-6">
+                                    <a className="projectHead"
+                                        href="https://born001.herokuapp.com/" target="_blank" rel="noreferrer"><h1 className="display-5 fw-bold lh-1 mb-3 animateLink">Newdle</h1></a>
+                                    <p className="lead">This is a voting application that allows users to create and vote on polls to help them make group decisions. Perfect for those indecisive people or a large opinionated group. (HTML, CSS, JS, Bulma, node.js, SQL, express, graphQL, react)<a href="https://github.com/MiernickiElijah/breakoutRoomNomads"
+                                        target="_blank" rel="noreferrer">REPO</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+
+
+                        {/* <!--p4--> */}
+                        <article className="container col-xxl-8 px-4 py-5">
+                            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                                <div className="col-lg-6">
+                                    <a className="projectHead"
+                                        href="https://miernickielijah.github.io/Druid/" target="_blank" rel="noreferrer"><h1 className="display-5 fw-bold lh-1 mb-3 animateLink">Druid</h1></a>
+                                    <p className="lead">This is a live weather dashboard with current and 5 day forecast based on US
+                                        city. Type in your city and check out the weather on a clean UI with simple info.
+                                        (HTML, CSS, HTML,
+                                        jQuery, bootstrap, APIs) <a href="https://github.com/MiernickiElijah/Druid"
+                                            target="_blank" rel="noreferrer">REPO</a>
+                                    </p>
+                                </div>
+                                <div className="col-10 col-sm-8 col-lg-6">
+                                    <a href="https://miernickielijah.github.io/Druid/" target="_blank" rel="noreferrer">
+                                        <img className="d-block mx-lg-auto img-fluid pics"
+                                            src={storm} alt="storm"></img>
+                                    </a>
+                                    <figcaption>photo by Johannes Plenio on Unsplash</figcaption>
+                                </div>
+                            </div>
+                        </article>
+
+
+                        {/* <!--p3--> */}
                         <article className="container col-xxl-8 px-4 py-5">
                             <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                                 <div className="col-10 col-sm-8 col-lg-6">
@@ -57,51 +103,7 @@ function Projects() {
                             </div>
                         </article>
 
-                        {/* <!--P2--> */}
-                        <article className="container col-xxl-8 px-4 py-5">
-                            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-                                <div className="col-lg-6">
-                                    <a className="projectHead"
-                                        href="https://miernickielijah.github.io/Druid/" target="_blank" rel="noreferrer"><h1 className="display-5 fw-bold lh-1 mb-3 animateLink">Druid</h1></a>
-                                    <p className="lead">This is a live weather dashboard with current and 5 day forecast based on US
-                                        city. Type in your city and check out the weather on a clean UI with simple info.
-                                        (HTML, CSS, HTML,
-                                        jQuery, bootstrap, APIs) <a href="https://github.com/MiernickiElijah/Druid"
-                                            target="_blank" rel="noreferrer">REPO</a>
-                                    </p>
-                                </div>
-                                <div className="col-10 col-sm-8 col-lg-6">
-                                    <a href="https://miernickielijah.github.io/Druid/" target="_blank" rel="noreferrer">
-                                        <img className="d-block mx-lg-auto img-fluid pics"
-                                            src={storm} alt="storm"></img>
-                                    </a>
-                                    <figcaption>photo by Johannes Plenio on Unsplash</figcaption>
-                                </div>
-                            </div>
-                        </article>
-
-                        {/* <!--P3--> */}
-                        <article className="container col-xxl-8 px-4 py-5">
-                            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-                                <div className="col-10 col-sm-8 col-lg-6">
-                                    <a href="https://github.com/MiernickiElijah/Arcane" target="_blank" rel="noreferrer">
-                                        <img className="d-block mx-lg-auto img-fluid pics"
-                                            src={magic} alt="holding moon"></img>
-                                    </a>
-                                    <figcaption>photo by Jasper Benning on Unsplash</figcaption>
-                                </div>
-                                <div className="col-lg-6">
-                                    <a className="projectHead"
-                                        href="https://github.com/MiernickiElijah/Arcane" target="_blank" rel="noreferrer"><h1 className="display-5 fw-bold lh-1 mb-3 animateLink">Arcane(REPO)</h1></a>
-                                    <p className="lead">This is a README.md generator using node.js. Allowing a user to create an entire
-                                        professional README file via command line. Because who wants to type out an entire markdown every time?
-                                        (Node.js, JS, jQuery, HTML, markdown)
-                                    </p>
-                                </div>
-                            </div>
-                        </article>
-
-                        {/* <!--P4--> */}
+                        {/* <!--p6--> */}
                         <article className="container col-xxl-8 px-4 py-5">
                             <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                                 <div className="col-lg-6">
@@ -123,6 +125,28 @@ function Projects() {
                                 </div>
                             </div>
                         </article>
+
+                        {/* <!--p5--> */}
+                        <article className="container col-xxl-8 px-4 py-5">
+                            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                                <div className="col-10 col-sm-8 col-lg-6">
+                                    <a href="https://github.com/MiernickiElijah/Arcane" target="_blank" rel="noreferrer">
+                                        <img className="d-block mx-lg-auto img-fluid pics"
+                                            src={magic} alt="holding moon"></img>
+                                    </a>
+                                    <figcaption>photo by Jasper Benning on Unsplash</figcaption>
+                                </div>
+                                <div className="col-lg-6">
+                                    <a className="projectHead"
+                                        href="https://github.com/MiernickiElijah/Arcane" target="_blank" rel="noreferrer"><h1 className="display-5 fw-bold lh-1 mb-3 animateLink">Arcane(REPO)</h1></a>
+                                    <p className="lead">This is a README.md generator using node.js. Allowing a user to create an entire
+                                        professional README file via command line. Because who wants to type out an entire markdown every time?
+                                        (Node.js, JS, jQuery, HTML, markdown)
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+
                     </section>
                 </div>
                 <footer id="footer">
