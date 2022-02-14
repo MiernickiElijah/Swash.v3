088c8248f84b2af137ca63337945413723cc6e7e
Elijah Miernicki <miernickielijah@gmail.com>
2022-02-14 16:47:01 -0600

    imported bootstrap and changed contact page grid

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,76 +7,78 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function Contact() {
     return (
-        <section className='card-group'>
-            <article className='contactColumn people card border-0'>
-                <div className="flip-card">
-                    <h3 className='contactName card-header-title'>GITHUB</h3>
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <FontAwesomeIcon className='icon profilepics card-image' id='cardIcon' icon={faGithub} alt='github' />
-                        </div>
-                        <div className="flip-card-back">
-                            <ul>
-                                <div><a className="contactLink aL" href="https://github.com/miernickielijah" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon className='icon' icon={faGithub} />
-                                    <strong>github.com/miernickielijah</strong></a></div>
-                            </ul>
+        <section className='flex-container'>
+            <div className='row'>
+                <article className='contactColumn people card border-0'>
+                    <div className="flip-card">
+                        <h3 className='contactName card-header-title'>GITHUB</h3>
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <FontAwesomeIcon className='icon profilepics card-image' id='cardIcon' icon={faGithub} alt='github' />
+                            </div>
+                            <div className="flip-card-back">
+                                <ul>
+                                    <div><a className="contactLink aL" href="https://github.com/miernickielijah" target="_blank" rel="noopener noreferrer">
+                                        <FontAwesomeIcon className='icon' icon={faGithub} />
+                                        <strong>github.com/miernickielijah</strong></a></div>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
+                </article>
 
-            <article className='contactColumn people card border-0'>
-                <div className="flip-card">
-                    <h3 className='contactName card-header-title'>EMAIL</h3>
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <FontAwesomeIcon className='icon profilepics card-image' id='cardIcon' icon={faInbox} alt='email' />
-                        </div>
-                        <div className="flip-card-back">
-                            <ul>
-                                <div><a className="contactLink aL" href="mailto:miernickielijah@gmail.com">
-                                    <FontAwesomeIcon className='icon' icon={faInbox} /><strong>miernickielijah@gmail.com</strong></a></div>
-                            </ul>
+                <article className='contactColumn people card border-0'>
+                    <div className="flip-card">
+                        <h3 className='contactName card-header-title'>EMAIL</h3>
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <FontAwesomeIcon className='icon profilepics card-image' id='cardIcon' icon={faInbox} alt='email' />
+                            </div>
+                            <div className="flip-card-back">
+                                <ul>
+                                    <div><a className="contactLink aL" href="mailto:miernickielijah@gmail.com">
+                                        <FontAwesomeIcon className='icon' icon={faInbox} /><strong>miernickielijah@gmail.com</strong></a></div>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
+                </article>
 
-            <article className='contactColumn people card border-0'>
-                <div className="flip-card">
-                    <h3 className='contactName card-header-title'>LINKEDIN</h3>
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <FontAwesomeIcon className='icon profilepics card-image' id='cardIcon' icon={faLinkedin} alt='linkedin' />
-                        </div>
-                        <div className="flip-card-back">
-                            <ul>
-                                <div><a className="contactLink aL" href="https://www.linkedin.com/in/miernickielijah/" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon className='icon' icon={faLinkedin} /><strong>linkedIn.com/in/miernickielijah</strong></a></div>
-                            </ul>
+                <article className='contactColumn people card border-0'>
+                    <div className="flip-card">
+                        <h3 className='contactName card-header-title'>LINKEDIN</h3>
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <FontAwesomeIcon className='icon profilepics card-image' id='cardIcon' icon={faLinkedin} alt='linkedin' />
+                            </div>
+                            <div className="flip-card-back">
+                                <ul>
+                                    <div><a className="contactLink aL" href="https://www.linkedin.com/in/miernickielijah/" target="_blank" rel="noopener noreferrer">
+                                        <FontAwesomeIcon className='icon' icon={faLinkedin} /><strong>linkedIn.com/in/miernickielijah</strong></a></div>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
+                </article>
 
-            <article className='contactColumn people card border-0'>
-                <div className="flip-card">
-                    <h3 className='contactName card-header-title'>Resume</h3>
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <FontAwesomeIcon class='icon profilepics card-image' id='cardIcon' icon={faFilePdf} alt='linkedin' />
-                        </div>
-                        <div className="flip-card-back">
-                            <ul>
-                                <a className="contactLink aL" href={resume} target="_blank" rel="noreferrer">
-                                    <FontAwesomeIcon class='icon' icon={faFilePdf} />
-                                    Resume</a>
-                            </ul>
+                <article className='contactColumn people card border-0'>
+                    <div className="flip-card">
+                        <h3 className='contactName card-header-title'>RESUME</h3>
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <FontAwesomeIcon class='icon profilepics card-image' id='cardIcon' icon={faFilePdf} alt='linkedin' />
+                            </div>
+                            <div className="flip-card-back">
+                                <ul>
+                                    <a className="contactLink aL" href={resume} target="_blank" rel="noreferrer">
+                                        <FontAwesomeIcon class='icon' icon={faFilePdf} />
+                                        Resume</a>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
+                </article>
+            </div>
         </section>
     )
 }
